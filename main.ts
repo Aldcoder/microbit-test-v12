d892a2db1f2dccab63ec74e0f80b89a577267174
Aldcoder <122543592+Aldcoder@users.noreply.github.com>
2023-01-19 09:55:27 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    music.setBuiltInSpeakerEnabled(true)
+    music.playMelody("D E D E F E F E ", 300)
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
+basic.forever(function () {
+	
+})
